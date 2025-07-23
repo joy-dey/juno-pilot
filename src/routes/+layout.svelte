e18d8e745967b21d4f361b9791528fc3811ed96b
joydey <joy.dey@xlayer.in>
2025-07-23 16:51:42 +0530

    feat(layout): add Navbar component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { Navbar } from '$lib';
+	import 'remixicon/fonts/remixicon.css';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
+<Navbar></Navbar>
 {@render children()}
